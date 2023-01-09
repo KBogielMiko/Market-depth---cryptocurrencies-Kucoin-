3df90970f6807bdb9baed99ea2eb5acabecd0afe
KBogielMiko <121521106+KBogielMiko@users.noreply.github.com>
2023-01-09 01:32:28 +0100

    Update Market depth - cryptocurrencies Kucoin.py

--- Market depth - cryptocurrencies Kucoin.py
+++ Market depth - cryptocurrencies Kucoin.py
@@ -1,52 +1,52 @@
-'''Market depth - cryptocurrencies (KuCoin)'''
-
-#Library import
-from kucoin.market import market
+import numpy as np
 import os
 import pandas as pd
-import numpy as np
+
+from kucoin.market import market
+
+# API parameters
 api_key = 'key'
 api_secret = 'secret'
 api_passphrase = 'passphrase'
 
-# API
+# API initialization
 market = market.MarketData(key = api_key, secret = api_secret, passphrase = api_passphrase)   
 
 
 # At the begining i'm going to try do it with a single coin
 order_book = market.get_aggregated_orderv3('BTC-USDT')  # API documentation
-order_book.keys()
 type(order_book)
+order_book.keys()
 
-# 'asks' (ask prices) are neccessary for further operation
+# 'asks' (ask prices) are only one neccessary key for further operation
 ask_btc = order_book['asks']
 ask_btc[:5]
 type(ask_btc)
 type(ask_btc[0][1])
 
 
-# df for implement column 'price'*'amount'
+# Create df for implement column 'price'*'amount'
 btc_df = pd.DataFrame(ask_btc, columns = ['price','amount'])
 btc_df.head()
 
-# we need floats for multiple columns 'price'*'amount'
+# Convert strings to floats for multiple columns 'price'*'amount'
 btc_df['price'] = btc_df['price'].astype(float) 
 btc_df['amount'] = btc_df['amount'].astype(float) 
 type(btc_df['price'][0])
 type(btc_df['amount'][0])
 
-# new column
+# Create new column
 btc_df['sum'] = btc_df['price']*btc_df['amount'] 
 btc_df.head()
 type(btc_df['sum'][0])
 
-#change to list for faster calculations
+# Convert to list for faster calculations
 btc_list = btc_df.values.tolist() 
 btc_list[:5]
 len(btc_list)
 
 ''' I want to check, on which order through orderbook we are out of money - 
-it means, that we gonna pass order after order till we spent whole 50K - with this way we gonna find a future price'''
+it means, that we gonna pass order after order till we spent whole 50K - with this way we are going to find a future price'''
 
 actual_price_btc = btc_list[0][0]
 cash = 2000000
@@ -60,15 +60,12 @@ for i in range(len(btc_list)):
         
 print(result)
 
-####### Now I'm gonna do the same operation for rest coins 
+####### Now I'm going to do the same operation for rest coins 
 
-#API
-from kucoin.client import Market
-
-# API documentation
+# API
 client = Market(url='https://api.kucoin.com')  
 
-#all tickers for make a list with only USDT tickers
+# Get all tickers for make a list with only USDT tickers
 
 #API doccumentation
 tickers = client.get_all_tickers()  
@@ -76,15 +73,12 @@ tickers = client.get_all_tickers()
 tickers.keys()
 tickers = pd.DataFrame(tickers['ticker'])
 tickers.head()
-
 tickers.set_index('symbol', inplace=True)
 tickers.head()
-
 list_tickers = list(tickers.index.values)
 list_tickers[:5]
 
 # only USDT tickers for further operations
-
 usdt_tickers = []
 for i in list_tickers:
     if 'USDT' in i:
@@ -98,27 +92,26 @@ len(usdt_tickers)
 orderbook_usdt = []
 for i in usdt_tickers:
     orderbook_usdt.append(market.get_aggregated_orderv3(i))   
-# sometimes there can be a problem with download all orderbooks - we receive an information 'Too many requests'. 
-# This is due to overload of Kucoin servers. In the future I will try to avoid this error with slowing down the program.
+# sometimes in line above there can be a problem with download all orderbooks - we receive an information 'Too many requests'. 
+# This is due to overload of Kucoin servers. In the future I will try to avoid this error with slowing down the program / using 'try' while looping
     
     
 len(orderbook_usdt)
 orderbook_usdt = orderbook_usdt[:-1]
-len(orderbook_usdt)
 orderbook_usdt[:1]
 
-# only asks - we are not interested in bids 
+# Again only asks - we are not interested in bids 
 orderbook_usdt_asks = []
 for i in orderbook_usdt:
        orderbook_usdt_asks.append(i['asks'])
 
-# convert every single orderbook to seperated df
+# Convert every single orderbook to seperated df
 orderbook_usdt_df = []
 for i in orderbook_usdt_asks:
     a = pd.DataFrame(i, columns = ['price','amount'])
     orderbook_usdt_df.append(a)
 
-# whole strings to floats for create 'sum' column
+# Strings to floats for create 'sum' column
 for i in orderbook_usdt_df:
     i['price'] = i['price'].astype(float)
     i['amount'] = i['amount'].astype(float)
